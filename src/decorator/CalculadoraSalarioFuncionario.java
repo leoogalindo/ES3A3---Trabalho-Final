e66a3c2166897503e3a749b1c0c22b507ce1b046
Ana Calixto <aninhacalixto@gmail.com>
2022-11-29 21:22:52 -0300

    Update CalculadoraSalarioFuncionario.java

--- src/decorator/CalculadoraSalarioFuncionario.java
+++ src/decorator/CalculadoraSalarioFuncionario.java
@@ -2,7 +2,9 @@ package decorator;
 
 public class CalculadoraSalarioFuncionario implements CalculadoraSalario {
 	public double calcularSalario(int horasTrabalhadas) {
+		
         final double valorHora = 15d;
         return valorHora * horasTrabalhadas;
+		
     }
 }
